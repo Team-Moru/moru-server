ff0ff7cc1c65246df460d6a610ee2a97965e86ec
민정 <litt22@g.hongik.ac.kr>
2026-07-13 23:08:00 +0900

    Revert "feat : 루틴 실행 결과 저장 API 추가"

--- src/main/java/com/moru/server/domain/routine/service/command/RoutineExecution/RoutineExecutionCommandServiceImpl.java
+++ src/main/java/com/moru/server/domain/routine/service/command/RoutineExecution/RoutineExecutionCommandServiceImpl.java
@@ -1,45 +1,4 @@
 package com.moru.server.domain.routine.service.command.RoutineExecution;
 
-
-
-import com.moru.server.domain.routine.converter.RoutineExecutionConverter;
-import com.moru.server.domain.routine.dto.RoutineExecutionRequestDTO;
-import com.moru.server.domain.routine.dto.RoutineExecutionResponseDTO;
-import com.moru.server.domain.routine.entity.Routine;
-import com.moru.server.domain.routine.entity.RoutineExecution;
-import com.moru.server.domain.routine.repository.RoutineExecutionRepository;
-import com.moru.server.domain.routine.repository.RoutineRepository;
-import com.moru.server.global.exception.BusinessException;
-import com.moru.server.global.response.code.status.ErrorStatus;
-import lombok.RequiredArgsConstructor;
-import org.springframework.stereotype.Service;
-import org.springframework.transaction.annotation.Transactional;
-
-@Service
-@Transactional
-@RequiredArgsConstructor
-public class RoutineExecutionCommandServiceImpl implements RoutineExecutionCommandService {
-
-    private final RoutineExecutionRepository routineExecutionRepository;
-    private final RoutineRepository routineRepository;
-
-
-
-    @Override
-    public RoutineExecutionResponseDTO.RoutineExecutionResultRes saveExecutionResult(Long memberId, RoutineExecutionRequestDTO.RoutineExecutionResultReq req) {
-
-        Routine routine = routineRepository.findById(req.routineId())
-                .orElseThrow(() -> new BusinessException(ErrorStatus.ROUTINE_NOT_FOUND));
-
-
-        if(!routine.getRoutineGroup().isOwnedBy(memberId)){
-            throw new BusinessException(ErrorStatus.ROUTINE_NOT_FOUND);
-        }
-
-        RoutineExecution routineExecution = RoutineExecutionConverter.toEntity(req,routine);
-        routineExecutionRepository.save(routineExecution);
-
-
-        return RoutineExecutionConverter.toResponse(routineExecution);
-    }
+public class RoutineExecutionCommandServiceImpl {
 }
