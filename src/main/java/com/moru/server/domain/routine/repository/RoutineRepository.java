31b4a4645eea487647c94921cef380dcd132916b
youngsun <sun010331@naver.com>
2026-07-13 12:10:25 +0900

    feat : 루틴 실행 결과 저장 API 추가

--- src/main/java/com/moru/server/domain/routine/repository/RoutineRepository.java
+++ src/main/java/com/moru/server/domain/routine/repository/RoutineRepository.java
@@ -1,4 +1,11 @@
 package com.moru.server.domain.routine.repository;
 
-public class RoutineRepository {
+
+import com.moru.server.domain.routine.entity.Routine;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+
+@Repository
+public interface RoutineRepository extends JpaRepository<Routine, Long> {
 }
--- src/main/java/com/moru/server/global/response/code/status/ErrorStatus.java
+++ src/main/java/com/moru/server/global/response/code/status/ErrorStatus.java
@@ -47,6 +47,12 @@ public enum ErrorStatus implements BaseCode {
     ROUTINE_GROUP_NOT_FOUND(HttpStatus.NOT_FOUND, "ROUTINE4002", "존재하지 않는 루틴 그룹입니다."),
     ROUTINE_GROUP_FORBIDDEN(HttpStatus.FORBIDDEN, "ROUTINE4003", "본인 소유의 루틴 그룹이 아닙니다."),
 
+    // 루틴 관련 에러
+    ROUTINE_NOT_FOUND(HttpStatus.NOT_FOUND,"ROUTINE4004","존재하지 않는 루틴입니다."),
+    ROUTINE_FORBIDDEN(HttpStatus.FORBIDDEN,"ROUTINE4005","본인 소유의 루틴이 아닙니다."),
+
+
+
     // TTS 관련 에러
     TTS_NOT_FOUND(HttpStatus.BAD_REQUEST, "TTS4001", "존재하지 않는 목소리입니다.");
 
