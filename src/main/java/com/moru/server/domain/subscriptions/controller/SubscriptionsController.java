0c8fa75d133b406172977d8d50b6e973a939cf45
cyunwook <cyunwook@gmail.com>
2026-07-24 20:47:13 +0900

    feat: 내 구독 정보 조회 API 추가

--- src/main/java/com/moru/server/domain/subscriptions/controller/SubscriptionsController.java
+++ src/main/java/com/moru/server/domain/subscriptions/controller/SubscriptionsController.java
@@ -1,4 +1,30 @@
 package com.moru.server.domain.subscriptions.controller;
 
+import com.moru.server.domain.subscriptions.dto.SubscriptionsResponseDTO;
+import com.moru.server.domain.subscriptions.service.query.SubscriptionsQueryService;
+import com.moru.server.global.response.ApiResponse;
+import com.moru.server.global.security.auth.AuthenticatedMember;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@Tag(name = "Subscriptions", description = "구독 API")
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/subscriptions")
 public class SubscriptionsController {
+
+    private final SubscriptionsQueryService subscriptionsQueryService;
+
+    @Operation(summary = "내 구독 정보 조회", description = "현재 로그인한 사용자의 구독 플랜 및 유효 기간을 조회합니다.")
+    @GetMapping("/me")
+    public ApiResponse<SubscriptionsResponseDTO.SubscriptionInfoResponse> getMySubscription(
+            @AuthenticationPrincipal AuthenticatedMember member
+    ) {
+        return ApiResponse.onSuccess(subscriptionsQueryService.getMySubscription(member.memberId()));
+    }
 }
--- src/main/java/com/moru/server/domain/subscriptions/service/query/SubscriptionsQueryService.java
+++ src/main/java/com/moru/server/domain/subscriptions/service/query/SubscriptionsQueryService.java
@@ -1,4 +1,8 @@
 package com.moru.server.domain.subscriptions.service.query;
 
+import com.moru.server.domain.subscriptions.dto.SubscriptionsResponseDTO;
+
 public interface SubscriptionsQueryService {
+
+    SubscriptionsResponseDTO.SubscriptionInfoResponse getMySubscription(Long memberId);
 }
